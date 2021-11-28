b67dd279b8b67656ab6f9bfe78d8df5653b4bd89
hareshnayak <sketchharry01@gmail.com>
2021-11-28 21:23:16 +0530

    function tasks added

--- src/tasks/3loops.kt
+++ src/tasks/3loops.kt
@@ -1,5 +1,6 @@
 package tasks
 
 fun main(){
-
+        // Task 1: Print first 10 odd numbers
+        // Task 2: Print prime number till 20
 }
---
+++ src/tasks/4functions.kt
@@ -0,0 +1,11 @@
+package tasks
+
+fun main(){
+    /* Task 1: Write a function to print pattern
+                       *
+                       **
+                       ***
+       Task 2: Write a function to print square of a number
+       Task 3: Write a function to return middle element of array
+     */
+}
